b80499ca6575ad8add0b9a2047307393ca120a10
k-ta-yamada <key.luvless@gmail.com>
2017-11-23 18:43:09 +0900

    [mod] roles/db_slave.rb

--- roles/db_slave.rb
+++ roles/db_slave.rb
@@ -3,3 +3,11 @@ include_recipe '../cookbooks/hosts'
 include_recipe '../cookbooks/postgresql'
 include_recipe '../cookbooks/postgresql/directory_archivedir'
 include_recipe '../cookbooks/postgresql/file_pgpass'
+# include_recipe '../cookbooks/postgresql/initdb'
+# include_recipe '../cookbooks/postgresql/conf'
+# include_recipe '../cookbooks/postgresql/create_role'
+# include_recipe '../cookbooks/postgresql/create_extension'
+
+service 'postgresql-9.6.service' do
+  action :stop
+end
