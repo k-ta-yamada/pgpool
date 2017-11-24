ce37fe65c755c55093a39f7cd419106d4f457d82
k-ta-yamada <key.luvless@gmail.com>
2017-11-24 10:58:49 +0900

    [fix] pgpool.conf: wd_hostname and more are static text #15

--- cookbooks/pgpool-II/conf.rb
+++ cookbooks/pgpool-II/conf.rb
@@ -1,30 +1,29 @@
+BACKEND_PREFIX = node[:common][:backend_prefix]
+
+template '/etc/pgpool-II/pgpool.conf' do
+  hostname = run_command("hostname").stdout.chomp
+  depend_on_hostname = node[:pgpool][:pgpool_conf][:depend_on_hostname][hostname]
+
+  variables backend_prefix: BACKEND_PREFIX,
+            pgpool_conf: node[:pgpool][:pgpool_conf],
+            depend_on_hostname: depend_on_hostname
+end
+
+directory node[:pgpool][:common][:rundir]
+directory node[:pgpool][:common][:logdir]
+
 remote_file '/etc/pgpool-II/failover.sh' do
-  # source '/etc/pgpool-II/failover.sh'
   owner 'root'
   group 'root'
   mode  '755'
 end
 
-directory node[:pgpool][:pgpool_conf][:failover_log_dir]
-
 node[:pgpool][:pcp_conf].each do |r|
   execute "echo #{r[:username]}:$(pg_md5 #{r[:md5auth]}) >> /etc/pgpool-II/pcp.conf"
 end
 
-# MEMO: role側でホストごとに指定
-# template '/etc/pgpool-II/pgpool.conf' do
-#   source '/etc/pgpool-II/pgpool.conf.erb'
-#   variables(pgpool_conf: node[:pgpool][:pgpool_conf],
-#             wd_hostname: 'pool1',
-#             heartbeat_destination0: 'pool2',
-#             heartbeat_destination1: 'pool3',
-#             other_pgpool_hostname0: 'pool2',
-#             other_pgpool_hostname1: 'pool3')
-# end
-
 template '/etc/pgpool-II/pool_hba.conf' do
-  # source '/etc/pgpool-II/pool_hba.conf.erb'
-  variables(pool_hba_conf: node[:pgpool][:pool_hba_conf])
+  variables pool_hba_conf: node[:pgpool][:pool_hba_conf]
 end
 
 node[:pgpool][:pool_passwd].each do |r|
